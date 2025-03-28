9cc9f720106ed314ab91c3d6c309a71623885927
abiparilla <abigailparilla06@gmail.com>
2025-03-28 14:51:25 -0400

    midt

---
+++ midterm/script.js
@@ -0,0 +1,9 @@
+console.log("CSS + JavaScript is powerful!");
+
+window.onload = function () {
+    alert("Welcome to Assignment 5 with JavaScript!");
+};
+
+function changeTheme() {
+    document.body.classList.toggle("dark-mode");
+}
